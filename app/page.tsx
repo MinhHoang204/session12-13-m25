6ed0665115d6f2fb5d0775d499bae77db0e9ed9f
Hoang <miinhhoangg204@gmail.com>
2024-08-27 09:19:44 +0700

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>page
+      {/*
+        reactjs 
+        cơ chế render dữ liệu Reactjs
+        nextjs
+        hỗ trợ vừa render dữ liệu ở phía client và phía server
+        NEXTJS:
+        1. server component: mặc định khi tạo trang
+        2. client component: dùng "use client" - khi nào nên dùng:
+        cần tương tác dữ liệu dùng (useState, useEffect, các sự kiện (onClick, onChange))
+
+        Cách render dữ liệu ở trong NEXTJS
+       */}
+    </div>
+  )
+}
